328f5c60d40aeae4a150aa22de65b81338f177bd
Ana Lopez-Mancisidor <alopezmancisidor@gmail.com>
2019-09-19 20:05:20 +0200

    new change for demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody ana demo!");
+  res.send("Hello from Appsody IBM Cloud Pack!");
 });
  
 module.exports.app = app;
